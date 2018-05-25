adcdcc34497803ddc743ab599daff5c6128fef24
Rafa Hernandez <rhernandez@teclib.com>
2018-05-25 15:18:48 -0400

    feat(app): first template code
    Signed-off-by: Rafa Hernandez <rhernandez@teclib.com>

---
+++ app/src/main/java/app/template/s42/templateandroid/MainActivity.java
@@ -0,0 +1,13 @@
+package app.template.s42.templateandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
