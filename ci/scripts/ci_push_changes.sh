92fedc049c2304833af5aadccde74b1841de4269
Naylin Medina <nmedina@teclib.com>
2018-06-07 15:26:40 -0400

    docs(header): add license to files
    Signed-off-by: Naylin Medina <nmedina@teclib.com>

--- ci/scripts/ci_push_changes.sh
+++ ci/scripts/ci_push_changes.sh
@@ -1,4 +1,29 @@
 #!/usr/bin/env bash
+#
+#  LICENSE
+#
+#  This file is part of Android Template App.
+#
+#  Android Template App is a subproject of Teclib.
+#
+#  Android Template App is free software: you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 3
+#  of the License, or (at your option) any later version.
+#
+#  Android Template App is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  ------------------------------------------------------------------------
+#  @author    Rafael Hernandez <rhernandez@teclib.com>
+#  @copyright Copyright (c) 2018 Teclib
+#  @license   GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
+#  @link      https://github.com/TECLIB/android-template-app/
+#  @link      https://teclib.github.io/android-template-app/
+#  @link      https://teclib-edition.com/en/
+#  ------------------------------------------------------------------------
+#
 
 # Push commits and tags to origin branch
 git push --follow-tags origin $CIRCLE_BRANCH
